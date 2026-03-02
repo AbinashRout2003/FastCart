8dd60fa5329f5f9a639d088da1fd337c3ad0436c
AbinashRout2003 <arout1604@gmail.com>
2026-03-03 01:52:24 +0530

    Fix Vercel MongoDB crash by removing dns.setServers

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
-import dns from "node:dns";
 
 export const connectDB = async () => {
   try {
-    dns.setServers(["8.8.8.8", "8.8.4.4"]);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected..");
   } catch (error) {
